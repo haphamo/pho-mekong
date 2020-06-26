b8253ce1914a9fbcf5595c088768c9f84aa48f6c
haphamo <hapham0392@gmail.com>
2020-06-26 15:41:28 -0400

    Commented out LandingPage and imprted MenuItem to preview the look while working on the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './Nav/Nav.js';
 import Landing from './LandingPage/Landing.js';
+import MenuItem from './MenuItem/MenuItem.js';
 
 function App() {
 	return (
 		<div className="App">
 			<Nav />
-			<Landing />
+			{/* <Landing /> */}
+			<MenuItem />
 		</div>
 	);
 }
