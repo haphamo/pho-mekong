5a83d527eb986a3f37164cb64a3fd9af4cfed063
haphamo <hapham0392@gmail.com>
2020-06-26 15:10:54 -0400

    Added template code from reactstrap's Card component to create the MenuItem component

---
+++ src/MenuItem/MenuItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody, CardLink,
+  CardTitle, CardSubtitle
+} from 'reactstrap';
+
+const MenuItem = (props) => {
+  return (
+    <div>
+      <Card>
+        <CardBody>
+          <CardTitle>Card title</CardTitle>
+          <CardSubtitle>Card subtitle</CardSubtitle>
+        </CardBody>
+        <img width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+        <CardBody>
+          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+          <CardLink href="#">Card Link</CardLink>
+          <CardLink href="#">Another Link</CardLink>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
+
+export default MenuItem;
