f322cf4ff0288eacc507baa71990c144b327b76b
haphamo <hapham0392@gmail.com>
2020-06-30 18:15:21 -0400

    renamed components, one is a single item, the other is the collection of them

--- src/MenuItem/MenuItem.js
+++ src/MenuItem/MenuItem.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import {
-  Card, CardImg, CardText, CardBody, CardLink,
-  CardTitle, CardSubtitle, Button, ButtonGroup
-} from 'reactstrap';
+import { ListGroup, ListGroupItem } from 'reactstrap';
 
-const MenuItem = (props) => {
-  return (
-      <Card style={{width: '30%', margin: '1em'}}>
-        <CardBody>
-          <CardTitle>P10. Pho Mekong</CardTitle>
-        </CardBody>
-        <section id="P10" style={{display: 'flex', justifyContent: 'center'}}>
-          <img width="90%" src="https://www.fodmapeveryday.com/wp-content/uploads/2017/06/Pho-closeup-copy-855x570.jpg" alt="Card image cap" /> 
-        </section>
-        <CardBody>
-          <CardText>Pho with assorted beef, rare, well done, beef balls, and assorted vegatables.</CardText>
-          <ButtonGroup style={{display: 'flex'}}>
-            <Button>Medium</Button>
-            <Button>Large</Button>
-            <Button>X-Large</Button>
-          </ButtonGroup>
-          {/* <Button color="primary">Add to Order</Button>{' '} */}
-        </CardBody>
-      </Card>
-  );
-};
+
+const MenuItem = ({item}) => {
+
+  const { code, name, price, desc, subOptions } = item;
+
+  const getAllSubOptions = (allOptions) => {
+    return allOptions.map(option => {
+      return(
+        <li>{option}</li>
+      )
+    })
+  }
+
+  return(
+    <ListGroupItem >
+      <section className='main' style={{display: 'flex', justifyContent: 'space-between'}}> 
+        <section className='item'>{code}. {name}</section>
+        <section className='price'>{price/100}</section>
+      </section>
+      {desc && <article className='description'>{desc}</article>}
+      {subOptions &&         
+        <ul>
+          {getAllSubOptions(subOptions)}
+        </ul>}
+    </ListGroupItem>
+  )
+}
 
 export default MenuItem;
