32f9b5ba9cef9ffaec0288bf8049f864e5a04a43
haphamo <hapham0392@gmail.com>
2020-06-30 13:59:23 -0400

    Added boilerplate code for menu list items

---
+++ src/ListItem/ListItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ListGroup, ListGroupItem } from 'reactstrap';
+
+const ListItem = () => {
+  return(
+    <ListGroup>
+      <ListGroupItem disabled tag="a" href="#">Cras justo odio</ListGroupItem>
+      <ListGroupItem tag="a" href="#">Dapibus ac facilisis in</ListGroupItem>
+      <ListGroupItem tag="a" href="#">Morbi leo risus</ListGroupItem>
+      <ListGroupItem tag="a" href="#">Porta ac consectetur ac</ListGroupItem>
+      <ListGroupItem tag="a" href="#">Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  )
+}
+
+export default ListItem;
