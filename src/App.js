ac0ff95ae52642654ea5940ab8879b6d04d6d01a
haphamo <hapham0392@gmail.com>
2020-06-30 18:17:24 -0400

    Refactored code so the AllMenuItems displays correctly

--- src/App.js
+++ src/App.js
@@ -2,20 +2,15 @@ import React from 'react';
 import Nav from './Nav/Nav.js';
 import Landing from './LandingPage/Landing.js';
 // import MenuItem from './MenuItem/MenuItem.js';
+import AllMenuItems from './AllMenuItems/AllMenuItems.js';
 
 function App() {
-
 
 	return (
 		<div className="App">
 			<Nav />
 			{/* <Landing /> */}
-				{/* <section style={{display: 'flex', flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-between'}}>
-					<MenuItem />
-					<MenuItem />
-					<MenuItem />
-					<MenuItem />
-				</section> */}
+				<AllMenuItems />
 		</div>
 	);
 }
