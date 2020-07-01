aeb6994b75914bc077267380b7f36e8c25f69f01
haphamo <hapham0392@gmail.com>
2020-06-30 23:35:15 -0400

    Updated component due to name change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Nav from './Nav/Nav.js';
 import Landing from './LandingPage/Landing.js';
-// import MenuItem from './MenuItem/MenuItem.js';
-import AllMenuItems from './AllMenuItems/AllMenuItems.js';
+import MenuSection from './MenuSection/MenuSection.js';
 
 function App() {
 
@@ -10,7 +9,7 @@ function App() {
 		<div className="App">
 			<Nav />
 			{/* <Landing /> */}
-				<AllMenuItems />
+				<MenuSection />
 		</div>
 	);
 }
