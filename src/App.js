ac01928c1fbd5716baef72484c1522c472653322
jenbeltran <francescajenbeltran@gmail.com>
2020-07-09 15:15:04 -0400

    add sidebar menu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import Nav from './Nav/Nav.js';
+import NavFullMenu from './NavFullMenu/FullMenu.js';
 import Landing from './LandingPage/Landing.js';
 import MenuSection from './MenuSection/MenuSection.js';
 import MenuItem from './MenuItem/MenuItem.js';
 import data from './data.js';
 
 function App() {
-
-	const allMenuItems = data.map(section => {
-		return(
-			<MenuSection section={section} />
-		)
+	const allMenuItems = data.map((section) => {
+		return <MenuSection section={section} />;
 	});
 
 	return (
 		<div className="App">
-			<Nav />
 			{/* <Landing /> */}
-      <section className="all-menu-items" style={{width: '55%'}}>
-			  {allMenuItems}
-      </section>
+			<section className="all-menu-items" style={{ width: '55%' }}>
+				{allMenuItems}
+			</section>
 		</div>
 	);
 }
