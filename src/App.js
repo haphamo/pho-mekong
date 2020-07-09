3a772355ba3c46823d5cbfac446300c100a99e13
haphamo <hapham0392@gmail.com>
2020-07-09 19:40:51 -0400

    Wrapped Banner component in a section with className pusher (from semantic iu) to have items side by side, no overlay or white space

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import NavFullMenu from './NavFullMenu/FullMenu.js';
 import Landing from './LandingPage/Landing.js';
 import MenuSection from './MenuSection/MenuSection.js';
 import MenuItem from './MenuItem/MenuItem.js';
+import Banner from './Banner/Banner.js';
+
 import data from './data.js';
 
 function App() {
@@ -12,11 +14,15 @@ function App() {
 
 	return (
 		<div className="App">
-			{/* <Landing /> */}
 			<NavFullMenu />
-			<section className="pusher" style={{width: '70%'}}>
+			{/* <Landing className="pusher"/> */}
+			{/* <section className="pusher" style={{width: '70%'}}>
 				{allMenuItems}
+			</section> */}
+			<section className="pusher">
+				<Banner />
 			</section>
+
 		</div>
 	);
 }
