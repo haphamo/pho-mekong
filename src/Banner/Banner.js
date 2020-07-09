457cb095dd4e77fd0fa3e01e02073d6fb14b6322
haphamo <hapham0392@gmail.com>
2020-07-09 19:39:45 -0400

    Created img element with test image and added inline styling temporarily

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PhoBanner from '../images/test-banner-pho.jpeg';
 
 const Banner = () => {
   return(
-    <h1>Banner</h1>
+    <img src={PhoBanner} alt="banner" style={{objectFit: 'cover',
+      height: '16em', width: '100%', objectPosition: '0% 0%', filter: 'brightness(0.8) opacity(0.6)'}}></img>
   )
 }
 
