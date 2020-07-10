d9fbd7aa94bc96d107bcbf108cf67a5cfbdf5ff1
haphamo <hapham0392@gmail.com>
2020-07-09 20:36:09 -0400

    Added styling for the banner and desc above the items

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PhoBanner from '../images/test-banner-pho.jpeg';
 
 const Banner = () => {
+  // TO-DO: Center content container
   return(
-    <img src={PhoBanner} alt="banner" style={{objectFit: 'cover',
-      height: '16em', width: '100%', objectPosition: '0% 0%', filter: 'brightness(0.8) opacity(0.6)'}}></img>
+    <img src={PhoBanner} alt="banner" style={{objectFit: 'none',
+      height: '16em', width: '100%', objectPosition: '75% 40%', filter: 'brightness(0.8) opacity(0.6)'}}></img>
   )
 }
 
