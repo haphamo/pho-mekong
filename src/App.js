1e158aaa308827e45511e0ccf8d837fcf9f9a6cd
haphamo <hapham0392@gmail.com>
2020-07-09 20:36:50 -0400

    Hard-coded desc and added temp inline styling for the desc

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Banner from './Banner/Banner.js';
 import data from './data.js';
 
 function App() {
+	// TOFIX: The content cuts at width: 686px,
 	const allMenuItems = data.map((section) => {
 		return <MenuSection section={section} />;
 	});
@@ -16,13 +17,15 @@ function App() {
 		<div className="App">
 			<NavFullMenu />
 			{/* <Landing className="pusher"/> */}
-			{/* <section className="pusher" style={{width: '70%'}}>
-				{allMenuItems}
-			</section> */}
+
 			<section className="pusher">
 				<Banner />
+				<h1 style={{position: 'absolute', right: '50%', top: '25%'}}>PHO</h1>
+				<section className="description" style={{height: '7em', width: '76%', position: 'relative', right: '-2%', textAlign: 'justify', display: 'flex', alignItems: 'center'}}>Pho is a popular Vietnamese noodle soup. Beef bones are simmered for many hours in combination with herbs and spices that help to bring out the flavour. It is served with traditional rice noodles and your choice of meat, or vegetables. Topped with green and white onions and black pepper.</section>
 			</section>
-
+			<section className="pusher" style={{width: '80%'}}>
+				{allMenuItems}
+			</section>
 		</div>
 	);
 }
